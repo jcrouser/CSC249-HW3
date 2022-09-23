298c4d0ca3874ec495cdcc1d49abcb077653e828
R. Jordan Crouser <jcrouser@smith.edu>
2022-09-23 08:46:07 -0400

    Added attribution comment

--- ICMPtraceroute.py
+++ ICMPtraceroute.py
@@ -1,3 +1,6 @@
+# Attribution: this assignment is based on ICMP Traceroute Lab from Computer Networking: a Top-Down Approach by Jim Kurose and Keith Ross. 
+# It was modified for use in CSC249: Networks at Smith College by R. Jordan Crouser in Fall 2022
+
 from socket import *
 from ICMPpinger import checksum
 import os
